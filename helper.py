94f296a739b61ceb1de4c396229f5ab106b49d51
akashghosh20 <easystudy20@gmail.com>
2023-08-31 02:20:39 +0600

    a

--- helper.py
+++ helper.py
@@ -1,3 +1,6 @@
+from wordcloud import WordCloud
+import pandas as pd
+import re
 def fetchStat(selected_user,df):
     if selected_user!="Overall":
         df = df[df['user'] == selected_user]
@@ -8,16 +11,100 @@ def fetchStat(selected_user,df):
 
     num_media_messages = df[df['message'] == '<Media omitted>'].shape[0]
     return num_messages, len(words),num_media_messages
-    # if selected_user=="Overall":
-    #     num_messages = df.shape[0]
-    #     words = []
-    #     for message in df['message']:
-    #         words.extend(message.split())
-    #     return num_messages,len(words)
-    # else:
-    #     new_df = df[df['user']==selected_user]
-    #     num_messages = new_df.shape[0]
-    #     words = []
-    #     for message in new_df['message']:
-    #         words.extend(message.split())
-    #     return num_messages,len(words)
+
+def MostBusiest(df):
+    x = df['user'].value_counts().head()
+    df = round((df['user'].value_counts()/df.shape[0])*100,2).reset_index().rename(columns={'index':'name','user':'percentage'})
+    return x,df
+
+def create_wordcloud(selected_user,df):
+    if selected_user!="Overall":
+        df = df[df['user'] == selected_user]
+
+    wc = WordCloud(width=500,height=500,min_font_size=10,background_color='white')
+    df_wc = wc.generate(df['message'].str.cat(sep=" "))
+    return df_wc
+
+def MostCommonWord(selected_user,df):
+    if selected_user!="Overall":
+        df = df[df['user'] == selected_user]
+
+    words = []
+
+    for message in df['message']:
+        words.extend(message.split())
+
+    from collections import Counter
+    return_df = pd.DataFrame(Counter(words).most_common(20))
+    return return_df
+
+# def count_emojis(selected_user,df,message):
+#     if selected_user!="Overall":
+#         df = df[df['user'] == selected_user]
+#     emoji_pattern = r'[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F\U0001FA70-\U0001FAFF]'
+#     emojis = re.findall(emoji_pattern, message)
+#     return emojis
+#
+#
+# # Example messages
+#     df = pd.DataFrame({'message': df['message']})
+#
+#     emoji_freq = {}
+#     for message in df['message']:
+#         emojis = count_emojis(message)
+#         for emoji in emojis:
+#             if emoji in emoji_freq:
+#                 emoji_freq[emoji] += 1
+#             else:
+#                 emoji_freq[emoji] = 1
+#
+#     emoji_df = pd.DataFrame({'Emoji': list(emoji_freq.keys()), 'Frequency': list(emoji_freq.values())})
+#     emoji_df = emoji_df.sort_values(by='Frequency', ascending=False)
+#     return emoji_df
+
+# In helper.py
+
+
+
+def count_emojis(message):
+    emoji_pattern = r'[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F\U0001FA70-\U0001FAFF]'
+    emojis = re.findall(emoji_pattern, message)
+    return emojis
+
+
+def emoji_frequency(selected_user, df):
+    if selected_user != "Overall":
+        df = df[df['user'] == selected_user]
+
+    emoji_freq = {}
+    for message in df['message']:
+        emojis = count_emojis(message)
+        for emoji in emojis:
+            if emoji in emoji_freq:
+                emoji_freq[emoji] += 1
+            else:
+                emoji_freq[emoji] = 1
+
+    emoji_df = pd.DataFrame({'Emoji': list(emoji_freq.keys()), 'Frequency': list(emoji_freq.values())})
+    emoji_df = emoji_df.sort_values(by='Frequency', ascending=False)
+    return emoji_df.head(6)
+
+def timeAnalysis(selected_user,df):
+    if selected_user != "Overall":
+        df = df[df['user'] == selected_user]
+
+    df['month_num'] = df['message_date'].dt.month
+    timeline = df.groupby(['year', 'month_num', 'month']).count()['message'].reset_index()
+    times = []
+    for i in range(timeline.shape[0]):
+        times.append(timeline['month'][i] + "-" + str(timeline['year'][i]))
+
+    timeline['time'] = times
+    return timeline
+def daily_timeline(selected_user,df):
+    if selected_user != "Overall":
+        df = df[df['user'] == selected_user]
+
+    df['only_date'] = df['message_date'].dt.date
+    daily_timeline = df.groupby('only_date').count()['message'].reset_index()
+    return daily_timeline
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import streamlit as st
 import preprocessing
 import helper
+import matplotlib.pyplot as plt
+import wordcloud
 st.sidebar.title("Whatsapp Chat Analyzer")
 uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     datas = bytes_data.decode('utf-8')
     df = preprocessing.preprocess(datas)
-    # st.dataframe(df)
+    st.dataframe(df)
     # fetch unique user
     user_list = df['user'].unique().tolist()
     user_list.remove('group_notification')
@@ -31,3 +33,60 @@ if uploaded_file is not None:
         with col3:
             st.header("Media Shared")
             st.title(num_of_media)
+
+        if selected_user == "Overall":
+            st.title("Most Busy Users")
+            x,new_df = helper.MostBusiest(df)
+            fig,ax = plt.subplots()
+            col1,col2 = st.columns(2)
+            with col1:
+                ax.bar(x.index, x.values,color='red')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+
+            with col2:
+                st.dataframe(new_df)
+
+        st.title("Time Analysis")
+        timeline = helper.timeAnalysis(selected_user,df)
+        fig,ax = plt.subplots()
+        ax.plot(timeline['time'], timeline['message'])
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+
+        st.title("Word Cloud")
+        df_wc = helper.create_wordcloud(selected_user,df)
+        fig,ax = plt.subplots()
+        ax.imshow(df_wc)
+        st.pyplot(fig)
+
+        most_common_df = helper.MostCommonWord(selected_user,df)
+        fig,ax = plt.subplots()
+        ax.barh(most_common_df[0],most_common_df[1])
+        plt.xticks(rotation='vertical')
+        st.title("Most Common Words")
+        st.pyplot(fig)
+
+
+        # counting emoji
+        result_df = helper.emoji_frequency(selected_user, df)
+        st.title("Emoji Analysis")
+
+        col1,col2 = st.columns(2)
+        with col1:
+            st.dataframe(result_df)
+        with col2:
+            fig,ax = plt.subplots()
+            custom_colors = ['#FF9999', '#66B3FF', '#99FF99', '#FFCC99', '#c2c2f0', '#ffb3e6']
+            ax.pie(result_df['Frequency'],labels=result_df['Emoji'],colors=['red','green','blue','violet','indigo','yellow'],autopct="%0.2f")
+            st.pyplot(fig)
+
+
+        st.title("Daily Timeline analysis")
+        daily_timeline = helper.daily_timeline(selected_user,df)
+        fig,ax = plt.subplots()
+        plt.figure(figsize=(18, 10))
+        plt.xticks(rotation='vertical')
+        ax.bar(daily_timeline['only_date'], daily_timeline['message'])
+        st.pyplot(fig)
